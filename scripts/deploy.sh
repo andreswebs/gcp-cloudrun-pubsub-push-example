c0d97b5e25071896501222f91d50e9b27bb2cce7
Andre Silva <andreswebs@pm.me>
2022-08-08 10:47:37 -0300

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # shellcheck disable=SC1090,SC2005
 
@@ -13,4 +13,6 @@ IMAGE_VERSION="${2:-latest}"
 
 IMAGE_TAG="$("${_SCRIPT_DIR}/config.sh" "${IMAGE_NAME}" "${IMAGE_VERSION}")"
 
-gcloud builds submit --tag "${IMAGE_TAG}"
+gcloud run deploy \
+  --image "${IMAGE_TAG}" \
+  --platform managed
