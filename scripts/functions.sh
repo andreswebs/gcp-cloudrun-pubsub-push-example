1e53e50a27163d96a2d8985ed805cd13ebc425f0
Andre Silva <andreswebs@pm.me>
2022-09-05 09:08:37 -0300

    update scripts

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -3,8 +3,8 @@ function docker_run_gcloud {
   docker run \
     -v "$HOME/.config/gcloud:/gcp/config:ro" \
     -v /gcp/config/logs \
+    --env CLOUDSDK_CORE_PROJECT \
     --env CLOUDSDK_CONFIG="/gcp/config" \
     --env GOOGLE_APPLICATION_CREDENTIALS="/gcp/config/application_default_credentials.json" \
-    --env GCLOUD_PROJECT \
     "${@}"
 }
